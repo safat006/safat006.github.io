8b2a5d8fe3b04ff498c9b9c013a8c283419e5df0
Safat Siddiqui <safat006@gmail.com>
2019-02-20 15:33:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ function getFrame() {
         const names = getClassNames(indices)
 
 
-        document.getElementById('status').innerHTML = names[0];
+        document.getElementById('status').innerHTML = 'Predicted class';
         //set the table 
         //setTable(names, probs)
         var img = document.getElementById('img_target');
